20e24509e2e16141ce77f90c1e0b1370e77138a0
Suin Kang <69102083+suinkangme@users.noreply.github.com>
2023-09-07 21:17:48 -0400

    Create email_log.sql

---
+++ SQL_Queries/email_log.sql
@@ -0,0 +1,45 @@
+INSERT INTO lac353_4.log VALUES ("2023-03-05","Montreal General Hospital","1650 Cedar 
+Ave",,"GOSR 8011 1233", "Ryan Gosling", "New Weekly Schedule", "Schedule from 2023-03-05  to 2023-03-12");
+
+INSERT INTO lac353_4.log VALUES ("2023-03-05","Montreal General Hospital","1650 Cedar Ave","MANL 9753 2718", "Lalisa Manoban", "New Weekly Schedule", "Schedule from 2023-03-05  to 2023-03-12");
+
+INSERT INTO lac353_4.log VALUES 
+("2023-03-05","Clinique CloudMed Westmount","4060 Saint-Catherine St W Suite 405","LAVD 4903 1729", "Daniel Lavoie", "New Weekly Schedule", "Schedule from 2023-03-05 to 2023-03-12");
+
+INSERT INTO lac353_4.log VALUES 
+("2023-03-05","Clinique CloudMed Westmount","4060 Saint-Catherine St W Suite 405","LIPD 9508 2200", "Dua Lipa", "New Weekly Schedule", "Schedule from 2023-03-05 to 2023-03-12");
+
+
+INSERT INTO lac353_4.log VALUES ("2023-03-05","CLSC Sainte-Catherine","66 St Catherine St E","LAVA 8459 2731", "Avril Lavigne", "New Weekly Schedule", "Schedule from 2023-03-05  to 2023-03-12");
+
+INSERT INTO lac353_4.log VALUES ("2023-03-05","CLSC Sainte-Catherine","66 St Catherine St E","DICL 7411 1100", "Leonardo Dicaprio", "New Weekly Schedule", "Schedule from 2023-03-05  to 2023-03-12");
+
+INSERT INTO lac353_4.log VALUES ("2023-03-12","CLSC Sainte-Catherine","66 St Catherine St E","DICL 7411 1100", "Leonardo Dicaprio", "New Weekly Schedule", "Schedule from 2023-03-13  to 2023-03-19");
+
+INSERT INTO lac353_4.log VALUES ("2023-03-12","CLSC Sainte-Catherine","66 St Catherine St E","LAVA 8459 2731", "Avril Lavigne", "New Weekly Schedule", "Schedule from 2023-03-13  to 2023-03-19");
+
+INSERT INTO lac353_4.log VALUES 
+("2023-03-12","Montreal General Hospital","1650 Cedar Ave","MANL 9753 2718", "Lalisa Manoban", "New Weekly Schedule", "Schedule from 2023-03-13 to 2023-03-19");
+
+INSERT INTO lac353_4.log VALUES 
+("2023-03-12","Montreal General Hospital","1650 Cedar Ave","WATE 9054 1521", "Emma
+Watson", "New Weekly Schedule", "Schedule from 2023-03-13 to 2023-03-19");
+
+INSERT INTO lac353_4.log VALUES 
+("2023-03-12","Montreal General Hospital","1650 Cedar Ave","GOSR 8011 1233", "Ryan
+Gosling", "New Weekly Schedule", "Schedule from 2023-03-13 to 2023-03-19");
+
+INSERT INTO lac353_4.log VALUES 
+("2023-03-12","Clinique CloudMed Westmount","4060 Saint-Catherine St W Suite 405","LAVD 4903 1729", "Daniel Lavoie", "New Weekly Schedule", "Schedule from 2023-03-13 to 2023-03-19");
+
+INSERT INTO lac353_4.log VALUES 
+("2023-03-12","Clinique CloudMed Westmount","4060 Saint-Catherine St W Suite 405","LIPD 9508 2200", "Dua Lipa", "New Weekly Schedule", "Schedule from 2023-03-13 to 2023-03-19");
+
+INSERT INTO lac353_4.log VALUES 
+("2023-03-13","CLSC Sainte-Catherine","66 St Catherine St E","LAVA 8459 2731", "Avril
+Lavigne", "Change of Schedule", "Since you reported a COVID-19 infection, your schedule for the upcoming two weeks has been canceled.");
+
+
+INSERT INTO lac353_4.log VALUES 
+("2023-03-13","Clinique CloudMed Westmount","4060 Saint-Catherine St W Suite 405","LAVD 4903 1729", "Daniel Lavoie", "Change of Schedule", "Since you reported a COVID-19 infection, your schedule for the upcoming two weeks has been canceled.");
+
